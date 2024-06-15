9328023246ec746e46a0a5321940a087279a417a
Mohan Jaiswal <129927767+webdevper@users.noreply.github.com>
2024-06-15 20:47:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var navLinks = document.getElementById( "navLinks");
+function showMenu(){
+    navLinks.style.transform = "translateX(0)"; // Move into view
+}
+function hideMenu(){
+    navLinks.style.transform = "translateX(100%)"; // Move out of view
+}
+
+
