09f702ed62207fcf9c1eb0b7ca6261acdecbea03
Mohan Jaiswal <webdevper.info@gmail.com>
2024-06-23 18:41:17 +0530

    add text effect

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ function hideMenu(){
     navLinks.style.transform = "translateX(100%)"; // Move out of view
 }
 
-
+var typed = new Typed("#element", {
+    strings: ["Mohan Jaiswal","WebDevper"],
+    typeSpeed: 150,
+    backSpeed: 90,
+    loop: true,
+    
+});
