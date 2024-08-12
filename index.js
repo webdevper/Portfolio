e13db8207aa8619033073e796b6c1d3c02ac54a0
Mohan Jaiswal <webdevper.info@gmail.com>
2024-08-12 19:58:38 +0530

    bug fix nav slider

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-var navLinks = document.getElementById( "navLinks");
-function showMenu(){
-    navLinks.style.transform = "translateX(0)"; // Move into view
-}
-function hideMenu(){
-    navLinks.style.transform = "translateX(100%)"; // Move out of view
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.transform = "translateX(0)"; 
+    navLinks.style.opacity = "1";  // Set opacity to 100%
+    navLinks.style.transition = "transform 0.5s ease, opacity 0.5s ease"; // Add transition for both transform and opacity
 }
 
+function hideMenu() {
+    navLinks.style.transform = "translateX(100%)"; 
+    navLinks.style.opacity = "0";  // Set opacity to 0%
+    navLinks.style.transition = "transform 0.5s ease, opacity 0.5s ease"; // Add transition for both transform and opacity
+}
+
+var menuLinks = document.querySelectorAll('.nav-links ul li a');
+menuLinks.forEach(link => {
+    link.addEventListener('click', hideMenu);
+});
+
 var typed = new Typed("#element", {
-    strings: ["Mohan Jaiswal","WebDevper"],
+    strings: ["Mohan Jaiswal", "WebDevper"],
     typeSpeed: 150,
     backSpeed: 90,
     loop: true,
-    
 });
